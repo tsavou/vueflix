bec252e205428445f2bf4d62c50a8d71bfec995e
Theo S <theosavourat@gmail.com>
2023-08-22 14:41:44 +0200

    api + movieCount footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,11 @@
 <script setup>
-import { ref } from 'vue';
+import { $fetch } from 'ohmyfetch'
+import { ref } from 'vue'
 
 const year = new Date().getFullYear();
-const movieCount = 24686;
+const movieCount = ref(0);
+
+$fetch('https://api.vueflix.boxydev.com/movies').then(response => {movieCount.value = response.length})
 
 </script>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,28 @@
+<script setup>
+// import { $fetch } from 'ohmyfetch'
+// import { onBeforeMount, ref } from 'vue'
+
+// const movies = ref([])
+// const loading = ref(true)
+// // Mode asynchrone
+// $fetch('https://api.vueflix.boxydev.com/movies').then(response => {
+//   setTimeout(() => {
+//     movies.value = response
+//     loading.value = false
+//   }, 3000)
+// })
+
+// // Mode synchrone
+// const genres = ref([]);
+// onBeforeMount(async () => {
+//   genres.value = await $fetch('https://api.vueflix.boxydev.com/genres')
+// })
+</script>
+
 
 <template>
   <h1 class="title">Accueil</h1>
+  <!-- <p v-if="loading">Chargement en cours...</p>
+  <p v-else v-for="movie in movies"> {{ movie.original_title }} genres</p> -->
+  
 </template>
