76e414cadb6fdc54b4746bb55b3dcc5297bc7167
Theo S <theosavourat@gmail.com>
2023-08-22 17:02:06 +0200

    ajout films +style

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,58 @@
+<script setup>
+
+
+const props = defineProps (['movie'])
+const note =parseInt(props.movie.vote_average * 10)
+const color= note >=70 ?  'green': note<40 ?'red' : 'yellow'
+
+</script>
+
+<template>
+    <figure>
+        <div class="relative">
+            <img :src="movie.poster_path" alt="">
+            <div class="absolute">
+                 <span class="note" :style="{ border: `solid 2px ${color}` }"> {{ note }}%</span>
+            </div>           
+        </div>
+        <figcaption>
+            <h3>{{ movie.title }}</h3>
+            <p>{{movie.release_date}}</p>
+        </figcaption>
+    </figure>
+</template>
+
+<style scoped>
+
+
+.relative{
+    position: relative;
+}
+
+.absolute{
+    position: absolute;
+    bottom: 3%;
+    left: 3%;
+}
+.note {
+    border-radius: 50%;
+    background-color: #000000;
+    color:#fff;
+    width: 40px;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+
+}
+
+img{
+    width: 100%;
+    border-radius:10px 10px 0 0 ;
+}
+
+figcaption{
+    padding: 0.5em;
+}
+</style>
--- src/views/FilmView.vue
+++ src/views/FilmView.vue
@@ -1,3 +1,45 @@
-<template>        
+<script setup>
+import MovieCard from '../components/MovieCard.vue';
+import { ref } from 'vue'
+import { $fetch } from 'ohmyfetch'
+
+const movies = ref([])
+const loading = ref(true)
+
+$fetch('http://localhost:3000/movies ')
+    .then(response => {
+        setTimeout(() => {
+            movies.value = response
+            loading.value = false
+        }, 0)
+    })
+
+</script>
+
+<template>
     <h1 class="title">Films</h1>
+    <p v-if="loading">Chargement en cours...</p>
+    <div v-else class="movie-container">
+        <MovieCard class="movie-card" v-for="movie in movies" :movie="movie" />
+    </div>
 </template>
+
+<style scoped>
+
+.movie-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    gap: 2%;
+    
+    
+}
+
+.movie-card {
+   width: 18%;
+   background-color: #fff;
+   margin-bottom: 2%;
+   border-radius: 10px;
+}
+
+</style>
