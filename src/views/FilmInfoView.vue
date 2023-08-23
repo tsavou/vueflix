2da5fb4afb493e2daa334525db9d90f404a5e64b
Theo S <theosavourat@gmail.com>
2023-08-23 23:13:54 +0200

    maj filminfo

--- src/views/FilmInfoView.vue
+++ src/views/FilmInfoView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { useRoute } from 'vue-router'
 import { $fetch } from 'ohmyfetch'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import Note from '@/components/Note.vue'
 
 const route = useRoute();
@@ -14,9 +14,8 @@ getMovie(route.params.id).then(response => {
     movie.value = response
 })
 
-const getReleaseYear = (date) => {
-    return new Date(date).getFullYear();
-}
+const Year = computed( () =>  new Date(movie.value.release_date).getFullYear());
+
 
 
 
@@ -28,20 +27,30 @@ const getReleaseYear = (date) => {
 </script>
 
 <template>
-    <div class="info-container" :style="{ backgroundImage: `url(${movie.backdrop_path})` }">
-        <img :src="movie.poster_path" :alt="movie.title">
-        <h2>{{ movie.title }}</h2><span>{{ getReleaseYear(movie.release_date) }}</span>
-        <p>{{ movie.release_date }}-{{movie.genre.name}}-{{ movie.runtime}} min </p>
-        <Note :note="movie.vote_average"/>
-        <a href=""><img src="../assets/svg/play.svg" alt="play trailer"> Voir la bande annonce</a>
+    <div class="movie-container" :style="{ backgroundImage: `url(${movie.backdrop_path})` }">
+        <div class="movie-bg">
+            <div class="container">
+
+                <img class="movie-poster" :src="movie.poster_path" :alt="movie.title">
+
+                <div class="movie-content">
 
 
-        <p>{{ movie.tagline }}</p>
-        <p>Synopsis</p>
-        <p>{{ movie.overview }}</p>
-    
+                    <h1>{{ movie.title }} <span>({{ Year }})</span></h1>
+                    <p>{{ movie.release_date }}-{{ movie.genre.name }}-{{ movie.runtime }} min </p>
+                    <Note :note="movie.vote_average" />
+                    <button><img src="../assets/svg/play.svg" alt="play trailer"> Voir la bande annonce</button>
 
-    
+
+                    <p>{{ movie.tagline }}</p>
+                    <p>Synopsis</p>
+                    <p>{{ movie.overview }}</p>
+                </div>
+
+            </div>
+        </div>
+
+
     </div>
     <div class="casting">
         <!-- composant acteur -->
@@ -49,14 +58,29 @@ const getReleaseYear = (date) => {
 </template>
 
 <style scoped>
-
-.info-container{
+.movie-container {
     background-size: cover;
     background-position: center;
 }
-a{
+
+.movie-bg {
+    background-color: rgba(0, 0, 0, 0.75);
+    padding: 0 40px;
+    color: #fff
+}
+
+.container{
     display: flex;
     align-items: center;
+    padding:40px 0;
+    gap: 40px;
+
 }
+
+.movie-poster{    
+    height: 400px;
+    width: auto;
+}
+
 
 </style>
